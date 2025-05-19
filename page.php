70d40cb3a31f7de7d1f8187adf81391190f528c6
Joan <papamundodev@gmail.com>
2025-05-19 09:46:34 +0200

    dropdown responsive (hover + details)

--- page.php
+++ page.php
@@ -16,7 +16,7 @@ $title = get_the_title();
                 <div class="page-title title-gradient">
                     <h1><?=$title; ?></h1>
                 </div><!-- End Page Title -->
-                <div class="img-container-logo-sm">
+                <div class="img-container-circle-logo-sm">
                     <a href="<?=home_url();?>" aria-label="<?=get_bloginfo('name');?>">
                         <img src="<?=get_template_directory_uri();?>/assets/images/logo.jpg" title="logo" alt="logo">
                     </a>
@@ -37,6 +37,47 @@ $title = get_the_title();
                 <div class="button-container-primary flex-center">
                     <button class="button">Button c'est un gros bouton graphique</button>
                 </div>
+                <nav aria-label="navigation" tabindex="0" class="dropdown-hover dropdown">
+                    <div class="dropdown-link ">
+                        <div class="img-container-circle-icon-sm">
+                            <div class="svg-container">
+                                <svg class="light-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>
+                                <svg class="dark-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                            </div>
+                        </div>
+                        <button class="dropdown-title btn button">This is a nice dropdown hover.</button> 
+                        
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><p>that allows the header/summary to be clickable.</p></li>
+                        <li><p>the content of the header is limited to 1 line and therefor the width of the dropdown is limited to the width of the container.
+                            You can always add a scroll if its really needed.
+                        </p></li>
+                        <li><p>ist supported in all platform</p></li>
+                        <li><a href="">you have to be carefull of the content under it and the superposition.</a></li>
+                        <li><a href="">links on 2 lignes look weirds, reduce the offset.</a></li>
+                    </ul>
+                </nav>
+                <details id="dropdown-hover" aria-label="navigation" tabindex="0" class="dropdown">
+                    <summary class="dropdown-link ">
+                        <div class="img-container-circle-icon-sm">
+                            <div class="svg-container">
+                                <svg class="light-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>
+                                <svg class="dark-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                            </div>
+                        </div>
+                        <span role="button" class="dropdown-title btn button">This is a nice dropdown hover.</span> 
+                    </summary>
+                    <ul class="dropdown-menu">
+                        <li><p>that allows the header/summary to be clickable.</p></li>
+                        <li><p>the content of the header is limited to 1 line and therefor the width of the dropdown is limited to the width of the container.
+                            You can always add a scroll if its really needed.
+                        </p></li>
+                        <li><p>ist supported in all platform</p></li>
+                        <li><a href="">you have to be carefull of the content under it and the superposition.</a></li>
+                        <li><a href="">links on 2 lignes look weirds, reduce the offset.</a></li>
+                    </ul>
+                </details>
             </div>
 
         </section>
@@ -369,8 +410,69 @@ $title = get_the_title();
                 </div>
             </div>
         </section>
+
         <section>
-            <div class="wrapper wrapper-border-inline">
+                    <div class="wrapper wrapper-border-inline dropdown-system">
+            <div class="design-system-header ">
+                <h2 class="">Dropdowns</h2>
+            </div>
+            <div class=" flex-auto">
+                <div>
+                    <div class="">
+                        <div class="">
+                            <h2 class="">Hover desktop</h2>
+                            <div class="flex-center">
+                                <nav id="dropdown-hover" aria-label="navigation" tabindex="0" class="dropdown-hover dropdown">
+                                    <div class="dropdown-link ">
+                                        <div class="img-container-circle-icon-sm">
+                                            <div class="svg-container">
+                                                <svg class="light-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>
+                                                <svg class="dark-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                                            </div>
+                                        </div>
+                                        <span role="button" class="dropdown-title btn button">This is a nice dropdown hover.</span> 
+                                    </div>
+                                    <ul class="dropdown-menu">
+                                        <li><p>that allows the header/summary to be clickable.</p></li>
+                                        <li><p>the content of the header is limited to 1 line and therefor the width of the dropdown is limited to the width of the container.
+                                            You can always add a scroll if its really needed.
+                                        </p></li>
+                                        <li><p>ist supported in all platform</p></li>
+                                        <li><a href="">you have to be carefull of the content under it and the superposition.</a></li>
+                                        <li><a href="">links on 2 lignes look weirds, reduce the offset.</a></li>
+                                    </ul>
+                                </nav>
+
+                                <details id="dropdown-hover" aria-label="navigation" tabindex="0" class="dropdown">
+                                    <summary class="dropdown-link ">
+                                        <div class="img-container-circle-icon-sm">
+                                            <div class="svg-container">
+                                                <svg class="light-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>
+                                                <svg class="dark-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                                            </div>
+                                        </div>
+                                        <span role="button" class="dropdown-title btn button">This is a nice dropdown hover.</span> 
+                                    </summary>
+                                    <ul class="dropdown-menu">
+                                        <li><p>that allows the header/summary to be clickable.</p></li>
+                                        <li><p>the content of the header is limited to 1 line and therefor the width of the dropdown is limited to the width of the container.
+                                            You can always add a scroll if its really needed.
+                                        </p></li>
+                                        <li><p>ist supported in all platform</p></li>
+                                        <li><a href="">you have to be carefull of the content under it and the superposition.</a></li>
+                                        <li><a href="">links on 2 lignes look weirds, reduce the offset.</a></li>
+                                    </ul>
+                                </details>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="wrapper wrapper-border-inline bg-primary-color-surface-1">
                 <div class="section-title">
                     <h2 class="">Button</h2>
                 </div>
